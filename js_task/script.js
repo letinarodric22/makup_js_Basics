7654218d424e104fc297476ba7752267cefc96c5
letinaroderic~ <letinaroderick@gmail.com>
2024-01-20 15:10:39 +0300

    2024 commits

---
+++ js_task/script.js
@@ -0,0 +1,43 @@
+//this is my first javascript code.
+console.log("hello world");
+let name= "sandaiya";
+console.log(name);
+// they should not be a reserved keyword.
+// they should not start with a capital letter, symbol  or a number.
+// always use meaningful words when naming variable.
+// they cannot contain a space or hyphen.
+// use CamelCasing..example letinaRodeickSandaiyaBabaMereso
+// case-sensitive.
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let myName = 'Rodric'; //string literal.
+let age = 24; //a number literal.
+let isApproved = false; // boolean literal.
+let firstName = undefined; // undefined.
+let selectedColor = null; // null.
+
+//REFERENCE DATA TYPES
+let person = {
+    nm:'mereso',
+    age:3
+};
+// dot notation
+ person.nm = 'kiberiti';
+ console.log(person);
+ console.log(person.age);
+//  bracket Notation.
+person['name'] = 'Saoli Letina';
+console.log(person['name']);
+
+
+// a function
+// - perform a task
+function gret(name1,name2){cd
+    console.log('hello ' + name1 + ' ' + name2);
+};
+gret('Shahabi', 'Karino');
+
+//  - calculate a value
